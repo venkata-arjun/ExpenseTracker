b119fe1805943bb02b1a2121f85fbec40e140911
Rankela Venkata Arjun <venkataarjun.r@gmail.com>
2026-03-17 03:50:17 +0530

    updated frontend

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 // utils/apiPaths.js
 export const API_PATHS = {
